9f9971dec63416a88eed651417420d168990617a
zeno-citiulo <zenocit02@gmail.com>
2024-11-18 17:47:18 +0100

    feat: exercise completed

---
+++ arrays/02-arrays-bonus/script.js
@@ -0,0 +1,19 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+// 1. Inverti l'ordine degli insegnanti nell'array teachers
+// e salva il risultato nella variabile reversedTeachers
+const reversedTeachers = null;
+
+// 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
+// con un nome di lunghezza maggiore o uguale a 5 caratteri
+const longNames = null;
+
+// 3. Rimuovi 'Ed' dall'array teachers
